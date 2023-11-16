5e1eb3b582b61e16cb295588c77c80168d65007e
felixxsp <147512817+felixxsp@users.noreply.github.com>
2023-11-16 20:34:56 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
-	connection "GladiResik/Connection"
 	food "GladiResik/Food"
+	"GladiResik/Orders"
+	"context"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	DB := options.Client().ApplyURI("mongodb://localhost:27017")
-	ClientDB := connection.NewDB(DB)
-	FoodData := food.Initialize(ClientDB.Client)
-	FoodData.PrintAll()
+	mainCtx := context.Background()
 
 	router := gin.Default()
-	connection.Setup(router, ClientDB, FoodData)
-	router.Run(":8080")
 
-	ClientDB.DisconnectDB()
+	client, _ := mongo.Connect(mainCtx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	database := client.Database("GladiResik")
+
+	FoodData := food.FDL_Init(mainCtx, database)
+	FUC := food.FUC_Init(mainCtx, FoodData)
+	FoodHandler := food.FHD_Init(router, FUC)
+	FoodHandler.Standby(mainCtx)
+
+	OrderData := Orders.ODL_Init(mainCtx, database)
+	OrderUC := Orders.OUC_Init(mainCtx, OrderData, FoodData)
+	OrderHandler := Orders.OHD_Init(router, OrderUC)
+	OrderHandler.Standby(mainCtx)
+
+	router.Run(":8080")
 }
