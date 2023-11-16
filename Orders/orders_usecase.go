f39d805511861673a4dc37d55163ed0b20f020b7
felixxsp <147512817+felixxsp@users.noreply.github.com>
2023-11-16 20:34:23 +0800

    Update orders_usecase.go

--- Orders/orders_usecase.go
+++ Orders/orders_usecase.go
@@ -1,1 +1,47 @@
 package Orders
+
+import (
+	entity "GladiResik/Entity"
+	"GladiResik/Food"
+	"context"
+
+	"github.com/google/uuid"
+)
+
+func GenerateUUID() string {
+	newUUID, err := uuid.NewRandom()
+	if err != nil {
+		panic(err)
+	}
+	return newUUID.String()
+}
+
+type OrderUsecase struct {
+	Datalayer *OrderData
+	FoodData  *Food.FoodData
+}
+
+func OUC_Init(ctx context.Context, data *OrderData, food *Food.FoodData) *OrderUsecase {
+	return &OrderUsecase{
+		FoodData:  food,
+		Datalayer: data,
+	}
+}
+
+func (usecase *OrderUsecase) Create(ctx context.Context, order []entity.Incoming) {
+	var NewOrder entity.Orders
+	for _, x := range order {
+		NewOrder.Uuid = GenerateUUID()
+		food, _ := usecase.FoodData.ViewOne(ctx, x.FoodID)
+		NewOrder.Contents = append(NewOrder.Contents, food)
+		NewOrder.Quantity = append(NewOrder.Quantity, x.Quantity)
+		NewOrder.Completion = append(NewOrder.Completion, false)
+
+	}
+	usecase.Datalayer.Insert(ctx, NewOrder)
+}
+
+func (usecase *OrderUsecase) ViewAll(ctx context.Context) []entity.Orders {
+	item, _ := usecase.Datalayer.ViewAll(ctx)
+	return item
+}
