d38f2ecd1e3817e4c69d13e658d705d41dd6e784
felixxsp <147512817+felixxsp@users.noreply.github.com>
2023-11-16 20:32:52 +0800

    Update orders.go

--- Entity/orders.go
+++ Entity/orders.go
@@ -1,7 +1,13 @@
 package entity
 
 type Orders struct {
-	Uuid       string `json:"uuid"`
-	Contents   []Food `json:"contents"`
-	Completion []bool `json:"completion"`
+	Uuid       string `json:"uuid" bson:"uuid"`
+	Contents   []Food `json:"contents" bson:"contents"`
+	Quantity   []int  `json:"qty" bson:"qty"`
+	Completion []bool `json:"completion" bson:"completion"`
+}
+
+type Incoming struct {
+	FoodID   int `json:"id" bson:"id"`
+	Quantity int `json:"qty" bson:"qty"`
 }
